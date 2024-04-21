7e5602b9dd78264f4b5725d4e264fc47bdfbe421
Tùng <letung49222@gmail.com>
2024-04-21 16:52:42 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Observable, combineLatest, forkJoin, map, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'test-angular';
+  arr: number[] = [];
+
+  ngOnInit() {
+
+  }
+
+  //Trường hợp tính tổng 3 số dạng observable
+  getSumObserve(): Observable<number> {
+    const A$: Observable<number> = of(1);
+    const B$: Observable<number> = of(2);
+    const C$: Observable<number> = of(9);
+
+    return combineLatest([A$, B$, C$]).pipe(map(([a, b, c]) => a + b + c + 10));
+  }
+
+  //Trường hợp tính tổng 3 số dạng observable
+  getArrObserve() {
+    const A$: Observable<number> = of(1);
+    const B$: Observable<number> = of(2);
+    const C$: Observable<number> = of(9);
+
+    combineLatest([A$, B$, C$]).subscribe(([A, B, C]) => {
+      this.arr.push(A);
+      this.arr.push(B);
+      this.arr.push(C);
+    });
+  }
 }
